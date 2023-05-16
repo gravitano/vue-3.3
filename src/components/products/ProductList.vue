208f9f64e1a62a5500097f552dd19766029f2f72
Warsono <warsono16694@gmail.com>
2023-05-16 14:18:44 +0700

    chore: add generic components example

---
+++ src/components/products/ProductList.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts" generic="T extends Product">
+import type { Product } from './types';
+
+defineProps<{
+  products: T[];
+}>();
+</script>
+
+<template>
+  <div>
+    <h1>Products</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        {{ product.title }}
+      </li>
+    </ul>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
 import AppButton from '@/components/app/AppButton.vue';
+import ProductList from '@/components/products/ProductList.vue';
+import type { Product } from '@/components/products/types';
+import { ref } from 'vue';
 
-//
+const products = ref<Product[]>([]);
+
+fetch('https://fakestoreapi.com/products')
+  .then((res) => res.json())
+  .then((data) => {
+    products.value = data as Product[];
+  });
 </script>
 
 <template>
   <h1>Welcome</h1>
+  <ProductList :products="products" />
   <AppButton>Click me</AppButton>
 </template>
