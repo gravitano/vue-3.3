22c4a44b2700a8d7672657c639b7e6b836e80623
Warsono <warsono16694@gmail.com>
2023-05-16 14:20:37 +0700

    chore: add defineSlots example

--- src/components/app/AppButton.vue
+++ src/components/app/AppButton.vue
@@ -2,10 +2,18 @@
 import type { ButtonProps } from './types';
 
 defineProps<ButtonProps>();
+
+defineSlots<{
+  default?: (props: ButtonProps) => any;
+  prefix?: (props: ButtonProps) => any;
+  suffix?: (props: ButtonProps) => any;
+}>();
 </script>
 
 <template>
   <button>
+    <slot name="prefix" />
     <slot />
+    <slot name="suffix" />
   </button>
 </template>
